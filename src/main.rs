42f1efdc8f34a0bfbb11e87e207b93ae9dc42edb
tuposilwe <rudigermkondya@gmail.com>
2026-01-04 22:01:32 +0300

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
 fn main() {
-    let rust = "\x52\x75\x73\x74";
-    println!("{}",rust);
+    // print_phase("Print my argument");
+    println!("{}",gcd(20, 5));
+}
+
+fn print_phase(phrase: &str) {
+    println!("{}",phrase);
+}
+
+fn gcd(mut a: u64,mut b:u64) -> u64{
+    while a != 0 {
+        if a < b{
+            let c = a;
+            a = b;
+            b = c;
+        }
+        a =  a % b;
+    }
+    b
 }
