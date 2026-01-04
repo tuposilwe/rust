2e231185a207cb0926a608ca1256c65242b97b43
tuposilwe <rudigermkondya@gmail.com>
2026-01-04 21:49:05 +0300

    string literals

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,4 @@
 fn main() {
-    let name  = String::from("Tyler");
-    let course = "Rust".to_string();
-    let new_name = name.replace("Ty","UI");
-
-    println!("{}",name);
-    println!("{}",course);
-    println!("{}",new_name);
-    
-    // &str = "string slice" or "stir"
-    let str1  = "hello"; //&str
-    let str2 = str1.to_string();
-    let str3 = &str2;
-    
-    println!("{}",str1);
-    println!("{}",str2);
-    println!("{}",str3);
-    
-    // compare string == != (does not equal)
-    println!("{}","ONE".to_lowercase() == "one");
+    let rust = "\x52\x75\x73\x74";
+    println!("{}",rust);
 }
