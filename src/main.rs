ed3488462a4d3c73b401f5e16651738c85a419bb
tuposilwe <rudigermkondya@gmail.com>
2026-01-04 21:44:44 +0300

    string

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
-    let v:Vec<i32> = (0..5).collect();
-    println!("{:?}",v);
+    let name  = String::from("Tyler");
+    let course = "Rust".to_string();
+    let new_name = name.replace("Ty","UI");
+
+    println!("{}",name);
+    println!("{}",course);
+    println!("{}",new_name);
     
-    let sv: &[i32] = &v[2..4];
-    println!("{:?}",sv);
+    // &str = "string slice" or "stir"
+    let str1  = "hello"; //&str
+    let str2 = str1.to_string();
+    let str3 = &str2;
+    
+    println!("{}",str1);
+    println!("{}",str2);
+    println!("{}",str3);
+    
+    // compare string == != (does not equal)
+    println!("{}","ONE".to_lowercase() == "one");
 }
