1c18209224ee69f2fa12c0e53cd812b2650b9838
tuposilwe <rudigermkondya@gmail.com>
2026-01-04 20:56:04 +0300

    slices

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
 fn main() {
-    let mut nums = vec![1,2,3];
-
-    nums.push(5);
-    println!("{:?}",nums);
-    
-    nums.pop();
-    println!("{:?}",nums);
-    
-    let mut vec = Vec::new();
-    vec.push("TEST");
-    vec.push("String");
-    println!("{:?}",vec);
-    
-    vec.reverse();
-    println!("{:?}",vec);
-    
-    let vect = Vec::<i32>::with_capacity(2);
-    println!("{:?}",vect.capacity());
-    
     let v:Vec<i32> = (0..5).collect();
     println!("{:?}",v);
     
-
-
+    let sv: &[i32] = &v[2..4];
+    println!("{:?}",sv);
 }
