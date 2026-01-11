c24ad79d52d263706d0ecad880b3ceecbd6755ff
tuposilwe <rudigermkondya@gmail.com>
2026-01-11 19:08:53 +0300

    methods

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,25 @@ struct User{
 }
 
 struct Coordinates(i32,i32,i32);
-
 struct UnitStruct;
+struct Square{
+    width: u32,
+    height: u32
+}
+
+impl Square {
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+
+    fn whats_my_width(&self) -> u32 {
+        self.width
+     }
+
+    fn change_width(&mut self,new_width: u32){
+        self.width = new_width;
+    }
+}
 
 fn main() {
     let user1 = User{
@@ -21,6 +38,14 @@ fn main() {
     println!("{}",user2.username);
 
     let cords = Coordinates(1,2,3);
+
+    let mut sq = Square{width:5,height:7};
+    
+    println!("{}",sq.area());
+    println!("{}",sq.whats_my_width());
+    sq.change_width(40);
+    println!("{}",sq.whats_my_width());
+    println!("{}",sq.area());
 }
 
 fn build_user(username: String) -> User{
