1f45d2da751b55cc26b9b4fdb8f21fd95930c823
tuposilwe <rudigermkondya@gmail.com>
2026-01-11 18:40:30 +0300

    Structs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
-fn main() {
-    let mut vec = vec![1,3,5,7];
-    println!("{:?}",check_val(&vec));
-    vec.push(15);
-    println!("{:?}",vec);
-
+struct User{
+    active: bool,
+    username: String,
+    sign_in_count: u32
 }
 
-fn check_val(val: &Vec<i8>)-> bool{
-    if val[0] == 1 {
-        return true;
-    }else{
-        return false;
+struct Coordinates(i32,i32,i32);
+
+struct UnitStruct;
+
+fn main() {
+    let user1 = User{
+        active: true,
+        username: String::from("Deskillz47"),
+        sign_in_count: 0
+    };
+
+    println!("{}",user1.username);
+    
+    let user2 = build_user(String::from("Rudiger"));
+    println!("{}",user2.username);
+
+    let cords = Coordinates(1,2,3);
+}
+
+fn build_user(username: String) -> User{
+    User {
+         active: true,
+         username,
+         sign_in_count: 1
     }
 }
