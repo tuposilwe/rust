0f78e9dbd493483383b6ad0b53e9a08cf59f56d4
tuposilwe <rudigermkondya@gmail.com>
2026-01-12 16:26:45 +0300

    Traits

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,43 @@ struct Point<T, U> {
     y: U,
 }
 
+trait Overview {
+    fn overview(&self) -> String {
+        String::from("This is a Rust course")
+    }
+}
+
+struct Course {
+    headline: String,
+    author: String,
+}
+struct AnotherCourse {
+    headline: String,
+    author: String,
+}
+
+impl Overview for Course {
+    // fn overview(&self) -> String {
+    //     format!("{}, {}", self.author, self.headline)
+    // }
+}
+impl Overview for AnotherCourse {
+    fn overview(&self) -> String {
+        format!("{}, {}", self.author, self.headline)
+    }
+}
+
 fn main() {
     let coord = Point { x: 5.0, y: "me" };
+    let course1 = Course {
+        headline: String::from("Headline!"),
+        author: String::from("Rudiger!"),
+    };
+    let course2 = AnotherCourse {
+        headline: String::from("Another Headline!"),
+        author: String::from("Deskillz47!"),
+    };
+
+    println!("{}", course1.overview());
+    println!("{}", course2.overview());
 }
