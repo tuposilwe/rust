a56a852a7e09e1ec6d8d4a7fd08ed4ce1037899c
tuposilwe <rudigermkondya@gmail.com>
2026-01-12 08:11:59 +0300

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,34 @@
-struct User{
-    active: bool,
-    username: String,
-    sign_in_count: u32
-}
+enum Pet {dog,cat,fish}
 
-struct Coordinates(i32,i32,i32);
-struct UnitStruct;
-struct Square{
-    width: u32,
-    height: u32
-}
-
-impl Square {
-    fn area(&self) -> u32{
-        self.width * self.height
-    }
-
-    fn whats_my_width(&self) -> u32 {
-        self.width
-     }
-
-    fn change_width(&mut self,new_width: u32){
-        self.width = new_width;
+impl Pet {
+    fn what_am_i(self) ->&'static str {
+        match self {
+            Pet::dog => "I am a dog",
+            Pet::cat => "I am a cat",
+            Pet::fish => "I am a fish",
+            
+        }
     }
 }
 
+enum IpAddrKind {
+    v4(String),
+    v6
+}
 
-struct MyString<'a>{
-    text: &'a str
+struct  IpAddr{
+    kind: IpAddrKind,
+    address: String
 }
 
 fn main() {
-    let user1 = User{
-        active: true,
-        username: String::from("Deskillz47"),
-        sign_in_count: 0
+    let my_pet = Pet::dog;
+    println!("{}",my_pet.what_am_i());
+
+    let home = IpAddrKind::v4(String::from("127.0.0.1"));
+    
+    let loopBack = IpAddr{
+        kind: IpAddrKind::v6,
+        address:String::from("::1")
     };
-
-    println!("{}",user1.username);
-    
-    let user2 = build_user(String::from("Rudiger"));
-    println!("{}",user2.username);
-
-    let cords = Coordinates(1,2,3);
-
-    let mut sq = Square{width:5,height:7};
-    
-    println!("{}",sq.area());
-    println!("{}",sq.whats_my_width());
-    sq.change_width(40);
-    println!("{}",sq.whats_my_width());
-    println!("{}",sq.area());
-    
-    let r;
-    
-    {
-        let x = 5;
-        r = x;
-    }
-
-    println!("lifetime {}",r);
-    
-    let str1 = String::from("This is my string");
-    let x  = MyString{text: str1.as_str()};
-    // println!("{:?}",x);
-
-    let s: &'static str = "I have static lifetime";
-
-
-}
-
-fn build_user(username: String) -> User{
-    User {
-         active: true,
-         username,
-         sign_in_count: 1
-    }
-}
-
-fn example<'a>(x: &'a str) -> &'a str{ // 'a = lifetime
-    x
 }
