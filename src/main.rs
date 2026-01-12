2a01e55fe67327f5b1cc32eaa34a22ec6a0e497d
tuposilwe <rudigermkondya@gmail.com>
2026-01-12 21:43:55 +0300

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,40 @@
-use rand::prelude::*;
-use std::collections::BinaryHeap;
-use std::collections::HashMap;
-
-use std::collections::HashSet;
+use std::fs::File;
+use std::io::ErrorKind;
+use std::fs::rename;
+use std::io::Error;
 
 fn main() {
-    let mut nums: Vec<i32> = vec![];
+    //panic!("panicked here!");
 
-    nums.push(1);
-    nums.push(2);
-    nums.push(3);
+    let vec = vec![1];
 
-    let pop = nums.pop(); //Option<T>, return None or Some(T)
+    let file = File::open("error.txt").expect("Error opening the file!");
+    // let file = match file {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("error.txt"){
+    //             Ok(file_created) => file_created,
+    //             Err(error) => panic!("Cannot create the file! {:?}",error),
+    //         }
+    //         _ => panic!("It was some other error kind!"),
+    //     }
+    // };
 
-    println!("{:?}", pop);
-
-    let two = nums[1]; //copy
-    // &nums[1], creates a reference if copy is not available
-
-    println!("{}", two);
-
-    let one = nums.first(); // return an Option<T>, so None if vec is empty, or Some<T> is [0]
-    println!("{:?}", one);
-
-    // .last
-    // .first_mut and .last_mut, so will borrow mutuable references
-
-    println!("{}", nums.len()); // return a value of the length
-    println!("{}", nums.is_empty()); //bool 
-
-    nums.insert(0, 10);
-    nums.insert(3, 12);
-    nums.insert(2, 25);
-
-    nums.remove(3);
-
-    nums.sort();
-    println!("{:?}", nums);
-
-    nums.reverse();
-    println!("{:?}", nums);
-
-    nums.shuffle(&mut rand::rng());
-    println!("{:?}", nums);
-
-    let mut bheap = BinaryHeap::new();
-
-    bheap.push(20);
-    bheap.push(1);
-    bheap.push(18);
-    bheap.push(5);
-
-    bheap.pop();
-
-    println!("{:?}", bheap);
-
-    println!("{:?}", bheap.peek()); // peek is going to return Option<T>, return None if empty, or Some(T) otherwise
-
-    println!("{:?}", bheap);
-
-    let mut hm = HashMap::new();
-
-    hm.insert(1, 1);
-    hm.insert(5, 2);
-    hm.insert(30, 3);
-    let old = hm.insert(30, 4); // key is going to upddate the old value of 3 to the new value of 4. also, going to return the old value
-
-    println!("{:?}", hm);
-    println!("{:?}", old);
-
-    println!("{}", hm.contains_key(&5));
-    println!("{:?}", hm.get(&5)); //returned Option<T>
-
-    let one = hm.remove(&1);
-    println!("{:?}", one);
-
-    let remove = hm.remove_entry(&5);
-    println!("{:?}", remove);
-
-    hm.clear();
-
-    println!("{}", hm.is_empty());
-
-    let mut hs = HashSet::new();
-
-    // len()
-    // is_empty()
-
-    hs.insert(1);
-    hs.insert(2);
-    hs.insert(3);
-    hs.insert(4);
-
-    // hs.remove(&2);
-
-    for x in hs.iter() {
-        println!("Iter: {}", x);
-    }
-
-    let mut hs2 = HashSet::new();
-
-    hs2.insert(1);
-    hs2.insert(3);
-    hs2.insert(5);
-    hs2.insert(7);
-
-    for x in hs.intersection(&hs2) {
-        println!("Intersection: {}", x);
-    }
-
-    let intersection = &hs & &hs2; // shorthand intersection using the binary bitwise & operator
-
-    for x in intersection {
-        println!("Short hand way: {}", x);
-    }
-
-    let union = &hs | &hs2;
-    for x in union {
-        println!("Union: {}", x);
-    }
-
-    let diff = &hs - &hs2;
-
-    for x in diff {
-        println!("Diff: {}", x);
-    }
+    // open_file().unwrap();
+     rename_file().unwrap();
 }
+
+fn open_file() -> Result<File, Error> {
+    let file = File::open("error.txt")?;
+    Ok(file)
+}
+
+fn rename_file() -> Result<(), Error> {
+    let file = rename("error.txt", "renamed.txt")?;
+    Ok(file)
+}
+
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
