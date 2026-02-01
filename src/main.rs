c2ebb5947a7e4b1fe7bfff69e5b2ee4f09bc47eb
tuposilwe <rudigermkondya@gmail.com>
2026-02-01 17:23:07 +0300

    Closures

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,61 @@
-mod find_and_replace;
+use rand::Rng;
+
+#[derive(Debug)]
+struct City {
+    city: String,
+    population: u64,
+}
+
+fn sort_pop(cities: &mut Vec<City>) {
+    // We can use a closure here
+    // cities.sort_by_key(|c| c.population);
+
+    cities.sort_by(|a, b| {
+        // First, compare populations descending
+        let res = b.population.cmp(&a.population);
+        if res == std::cmp::Ordering::Equal {
+            // If populations are equal, sort by name ascending
+            a.city.cmp(&b.city)
+        } else {
+            res
+        }
+    });
+}
 
 fn main() {
-    find_and_replace::run();
+    let mut rng = rand::rng();
+
+    // Changed to 'let mut' so we can sort it later
+    let mut cities = vec![
+        City {
+            city: "A".to_string(),
+            population: rng.random_range(0..=100),
+        },
+        City {
+            city: "B".to_string(),
+            population: rng.random_range(0..=100),
+        },
+        City {
+            city: "C".to_string(),
+            population: rng.random_range(0..=100),
+        },
+        City {
+            city: "D".to_string(),
+            population: rng.random_range(0..=100),
+        },
+        City {
+            city: "E".to_string(),
+            population: rng.random_range(0..=100),
+        },
+        City {
+            city: "F".to_string(),
+            population: rng.random_range(0..=100),
+        },
+    ];
+
+    // Sort the populated list
+    sort_pop(&mut cities);
+
+    // Print the list we actually put data into
+    println!("{:#?}", cities);
 }
