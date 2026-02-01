eb805ba563130d2dd1f09c63d7cb98f68ece532c
tuposilwe <rudigermkondya@gmail.com>
2026-02-01 14:50:47 +0300

    replacing text

--- src/find_and_replace.rs
+++ src/find_and_replace.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::env;
 use std::fs;
 use text_colorizer::*;
@@ -19,6 +20,11 @@ fn print_help() {
     eprintln!("Usage: <target string> <replacement string> <INPUT FILE> <OUTPUT FILE>");
 }
 
+fn replace(target: &str, rep: &str, data: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(data, rep).to_string())
+}
+
 fn read_and_write(args: &Arguments) {
     let data = match fs::read_to_string(&args.input_file) {
         Ok(f) => f,
@@ -33,7 +39,16 @@ fn read_and_write(args: &Arguments) {
         }
     };
 
-    match fs::write(&args.output_file, &data) {
+    let replace_data = match replace(&args.pattern, &args.replace,&data) {
+        Ok(d) => d,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}","Error".red().bold(),e);
+            std::process::exit(1);
+        }
+    };
+
+
+    match fs::write(&args.output_file, &replace_data) {
         Ok(_) => {}
         Err(e) => {
             eprintln!(
