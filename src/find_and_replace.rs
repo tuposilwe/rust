c62245dd297ebd7cefa57e0e8a5fbc8f7cf62101
tuposilwe <rudigermkondya@gmail.com>
2026-02-01 13:27:52 +0300

    Accessing Files

--- src/find_and_replace.rs
+++ src/find_and_replace.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use text_colorizer::*;
 
 #[derive(Debug)]
@@ -18,7 +19,35 @@ fn print_help() {
     eprintln!("Usage: <target string> <replacement string> <INPUT FILE> <OUTPUT FILE>");
 }
 
-pub fn run() {
+fn read_and_write(args: &Arguments) {
+    let data = match fs::read_to_string(&args.input_file) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file {} : {:?}",
+                "Error".red().bold(),
+                args.input_file,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output_file, &data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file {} : {:?}",
+                "Error".red().bold(),
+                args.input_file,
+                e
+            );
+            std::process::exit(1);
+        }
+    }
+}
+
+fn parse_args() -> Arguments {
     let args: Vec<String> = env::args().skip(1).collect();
     if args.len() != 4 {
         print_help();
@@ -29,4 +58,17 @@ pub fn run() {
         );
         std::process::exit(1);
     }
+
+    Arguments {
+        pattern: args[0].clone(),
+        replace: args[1].clone(),
+        input_file: args[2].clone(),
+        output_file: args[3].clone(),
+    }
+}
+
+pub fn run() {
+    let args = parse_args();
+    read_and_write(&args);
+    // print!("{:?}", args)
 }
