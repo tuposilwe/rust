e187ebbcd7562ad27477b5bb15967e67e098aa67
tuposilwe <rudigermkondya@gmail.com>
2026-02-07 08:15:20 +0300

    unsafe

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
-macro_rules! gcd {
-    ($a:expr,$b:expr) => {
-        {
-            let mut m = $b;
-            let mut n = $a;
+fn main() {
+    let mut num = 5;
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
 
-            while m != 0 {
-                if m < n{
-                    let t = m;
-                    m = n;
-                    n = t;
-                }
-                m = m % n;
-            }
-            n
-
-        }
-    };
-}
-
-
-fn main(){
-   println!("{}", gcd!(14,15));
-
+    unsafe {
+        println!("r1 is {:?}", *r1);
+        println!("r2 is {:?}", *r2);
+    }
 }
