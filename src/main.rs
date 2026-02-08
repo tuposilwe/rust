92ae8a5f2337e6b932c125c27bb42096a9e0ac42
tuposilwe <rudigermkondya@gmail.com>
2026-02-08 08:53:40 +0300

    Task,Future,Async and Await

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-fn main() {
-    let mut num = 5;
-    let r1 = &num as *const i32;
-    let r2 = &mut num as *mut i32;
+use async_std::{fs::File, io, prelude::*, task};
 
-    unsafe {
-        println!("r1 is {:?}", *r1);
-        println!("r2 is {:?}", *r2);
-    }
+async fn read_file(path: &str) -> io::Result<String> {
+    let mut file = File::open(path).await?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).await?;
+    Ok(contents)
+}
+
+fn main() {
+    let task = task::spawn(async {
+        let result = read_file("test.txt").await;
+        match result {
+            Ok(k) => println!("{}", k),
+            Err(e) => println!("Error reading from file: {}", e),
+        }
+    });
+
+    println!("Task has started!");
+    task::block_on(task);
+    println!("Stopped Task!");
 }
